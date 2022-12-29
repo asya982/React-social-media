0cab229aaf61cf122b2c75ecc94df01916e7dedc
Anastasiia <bagin.nst@gmail.com>
2022-12-29 15:20:58 +0200

    creating app

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Welcome</h1>
+            <button>home</button>
+            <button>feed</button>
+            <button>profile</button>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/Technologies.js
@@ -0,0 +1,13 @@
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>Js</li>
+                <li>Js</li>
+                <li>Js</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Technologies;
